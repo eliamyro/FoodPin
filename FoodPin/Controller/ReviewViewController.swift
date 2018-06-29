1019ded927afe0f25a8d77094f4265b340b8dff1
Elias Myronidis <elias.myronidis@hotmail.com>
2018-06-29 15:14:01 +0300

    • Added slide-in, spring and scale up animation to rate buttons.

--- FoodPin/Controller/ReviewViewController.swift
+++ FoodPin/Controller/ReviewViewController.swift
@@ -26,7 +26,13 @@ class ReviewViewController: UIViewController {
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
         
+        // Move the rate buttons outside of the screen
+        let moveRightTransform = CGAffineTransform(translationX: 600, y: 0)
+        let scaleUpTransform = CGAffineTransform(scaleX: 5.0, y: 5.0)
+        let moveScaleTransform = scaleUpTransform.concatenating(moveRightTransform)
+        
         for rateButton in rateButtons {
+            rateButton.transform = moveScaleTransform
             rateButton.alpha = 0.0
         }
 
@@ -34,11 +40,30 @@ class ReviewViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        UIView.animate(withDuration: 2.0) {
-            for rateButton in self.rateButtons {
-                rateButton.alpha = 1.0
-            }
-        }
+        UIView.animate(withDuration: 0.4, delay: 0.1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.1, options: [], animations: {
+            self.rateButtons[0].alpha = 1.0
+            self.rateButtons[0].transform = .identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.15, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.1, options: [], animations: {
+            self.rateButtons[1].alpha = 1.0
+            self.rateButtons[1].transform = .identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.20, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.1, options: [], animations: {
+            self.rateButtons[2].alpha = 1.0
+            self.rateButtons[2].transform = .identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.25, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.1, options: [], animations: {
+            self.rateButtons[3].alpha = 1.0
+            self.rateButtons[3].transform = .identity
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.30, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.1, options: [], animations: {
+            self.rateButtons[4].alpha = 1.0
+            self.rateButtons[4].transform = .identity
+        }, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
